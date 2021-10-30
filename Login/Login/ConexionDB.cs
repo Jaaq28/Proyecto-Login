4c5df254bf31faf938644cd1f22094dd62512fa6
Jaaq28 <90659536+Jaaq28@users.noreply.github.com>
2021-10-30 17:53:59 -0600

    login
    proyecto de login

---
+++ Login/Login/ConexionDB.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Datos;
+using System.Data.SqlClient;
+
+namespace Login
+{
+   public class ConexionDB
+    {
+        readonly string cadena = " Data Source=.;Initial Catalog=Login; Intregrated Security=true ";
+
+        public bool ValidarUsuario(Usuario user)
+        {
+            bool esUsuarioValido = false;
+
+            try
+            {
+                StringBuilder sql = new StringBuilder();
+                sql.Append(" SELECT 1 FROM USUARIO WHERE USUARIO = @User AND Clave = @Clave; ");
+
+                using (SqlConnection _conexion = new SqlConnection(cadena))
+                {
+                    _conexion.Open();
+                    using (SqlCommand comando = new SqlCommand(sql.ToString(), _conexion))
+                    {
+                        comando.CommandType = System.Data.CommandType.Text;
+                        comando.Parameters.Add(" @User ", System.Data.SqlDbType.NVarChar, 50).Value = user.CodigoUsuario;
+                        comando.Parameters.Add(" @Clave ", System.Data.SqlDbType.NVarChar, 50).Value = user.Clave;
+
+                        esUsuarioValido = Convert.ToBoolean(comando.ExecuteScalar());
+
+                    }
+
+                }
+
+            }
+            catch (Exception)
+            {
+                
+            }
+            return esUsuarioValido;
+
+        }
+    }
+
+}
---
+++ Login/Login/Form1.cs
@@ -0,0 +1,38 @@
+﻿using Datos;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Login
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        ConexionDB conexion = new ConexionDB();
+        Usuario user = new Usuario();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            bool valido = conexion.ValidarUsuario(user);
+
+            if (valido)
+            {
+                MessageBox.Show(" Usuario Correcto ");
+            }
+            else 
+            {
+                MessageBox.Show(" Datos de Usuario Incorrectos ");
+            }
+        }
+    }
+}
